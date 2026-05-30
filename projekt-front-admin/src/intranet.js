6802000390251044012df7c411891f205ffd9b87
isso2500 <isso2500@student.miun.se>
2026-05-30 13:28:29 +0200

    Initial commit.
    Index/login and subpage intranet created.
    Code implemented for login in main.js to post formData
    Code implemented for intranet to fetch user information, greeting them.

---
+++ projekt-front-admin/src/intranet.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log(`DOM Loaded`)
+    fetchEmployee()
+})
+
+async function fetchEmployee() {
+    try {
+        const token = localStorage.getItem("token")
+        if (!token) {
+            console.log(`Åtkomst nekad`)
+            return;
+        }
+
+
+        const response = await fetch(`http://127.0.0.1:3000/intranet`, {
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+
+        const data = await response.json();
+
+        const payload = JSON.parse(atob(token.split(".")[1]));
+
+        document.getElementById("welcomeHeader").innerHTML = `Välkommen ${data.firstname} ${data.surname}`
+    } catch (err) {
+        console.log(err)
+    }
+}
---
+++ projekt-front-admin/src/main.js
@@ -0,0 +1,78 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log(`Dom loaded`)
+
+    document.getElementById("loginBtn").addEventListener("click", signIn)
+})
+
+
+async function signIn(event) {
+    event.preventDefault()
+
+    const errors = [];
+
+    let errorList = document.getElementById("errorList")
+    errors.length = 0
+    errorList.innerHTML = "";
+    let username = document.getElementById("username").value
+    let password = document.getElementById("password").value
+
+    if (!username || !password) {
+        errors.push(`Användarnamn och lösenord måste fyllas i`)
+    }
+
+    if (errors.length > 0) {
+        errors.forEach(error => {
+            let errorLine = document.createElement("li")
+            errorLine.innerHTML = error
+            errorList.appendChild(errorLine)
+            return;
+        })
+    }
+
+    let formData = {
+        username: username,
+        password: password,
+    }
+
+    try {
+        let response = await fetch(`http://127.0.0.1:3000/login`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(formData)
+        })
+        if (!response.ok) {
+            result.errors.forEach(error => {
+                let errorLine = document.createElement("li")
+                errorLine.innerHTML = error
+                errorList.appendChild(errorLine)
+
+            });
+            return;
+        }
+   
+        const result = await response.json()
+
+        localStorage.setItem("token", result.token)
+
+        const token = localStorage.getItem("token")
+        const protResult = await fetch("http:127.0.0.1:3000/", {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+
+        const protData = protResult.json()
+
+        if (!protResult.ok) {
+            throw new Error(`Inloggning misslyckades`)
+        }
+
+        window.location.href = "/intranet"
+    } catch (err) {
+        console.error(err)
+    }
+
+
+}

3c5014b18cd502ced19d6f09fd2c0d5f6ccd5872
isso2500 <isso2500@student.miun.se>
2026-06-02 12:56:26 +0200

    htmls - Built subpages. added background to those missing it.
    Edit.js - Implemented code to find entry with corresponding ID and then PUTing update.
    
    add.js - Fetches all dishes.
    
    styles.css - Styled everything.

---
+++ projekt-front-admin/src/add.js
@@ -0,0 +1,110 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log(`DOM loaded`)
+
+    fetchMenu()
+})
+
+
+async function fetchMenu() {
+
+    try {
+        let db = await fetch("http://127.0.0.1:3000/add")
+        let result = await db.json()
+
+        let addMain = document.getElementById("addMain")
+        let mondayDiv = document.getElementById("monday")
+        let tuesdayDiv = document.getElementById("tuesday")
+        let wednesdayDiv = document.getElementById("wednesday")
+        let thursdayDiv = document.getElementById("thursday")
+        let fridayDiv = document.getElementById("friday")
+        let saturdayDiv = document.getElementById("saturday")
+        let sundayDiv = document.getElementById("sunday")
+
+        console.log(result)
+        result.forEach(dish => {
+            let dishWeekday = dish.weekday
+            let dishesDiv = document.createElement("div")
+            dishesDiv.setAttribute("class", "dishesDiv")
+            let dishWrapper = document.createElement("div")
+            dishWrapper.setAttribute("class", "dishWrapper")
+            let adminDishDiv = document.createElement("div")
+            adminDishDiv.setAttribute("class", "adminDishDiv")
+            let changeBtn = document.createElement("button")
+            changeBtn.addEventListener("click", () => {
+                changePage(dish._id)
+            })
+            changeBtn.setAttribute("class", "changeBtn")
+            changeBtn.style.backgroundColor = "orange"
+            changeBtn.textContent = `Redigera rätt`
+            changeBtn.setAttribute("data-id", dish._id)
+
+            let dishHeader = document.createElement("h3")
+            let dishIngrdnts = document.createElement("p")
+            let dishAlrgns = document.createElement("p")
+            let dishDiet = document.createElement("p")
+            let dishPrice = document.createElement("p")
+            let dishImage;
+            let dishDay = document.createElement("h4")
+
+            dishHeader.innerHTML = dish.dishname
+            dishIngrdnts.innerHTML = `Ingredienser: ${dish.ingredients}`
+            dishAlrgns.innerHTML = `Allergener: ${dish.allergens}`
+            dishDiet.innerHTML = dish.diet
+            dishPrice.innerHTML = `Pris: ${dish.price}kr`
+
+
+            adminDishDiv.appendChild(dishHeader)
+            adminDishDiv.appendChild(dishIngrdnts)
+            adminDishDiv.appendChild(dishAlrgns)
+            adminDishDiv.appendChild(dishDiet)
+            adminDishDiv.appendChild(dishPrice)
+            adminDishDiv.appendChild(changeBtn)
+
+
+            if (dishWeekday == 1) {
+                dishDay.innerHTML = "Måndagar"
+                mondayDiv.appendChild(dishWrapper)
+                adminDishDiv.appendChild(dishDay)
+                dishWrapper.appendChild(adminDishDiv)
+            } else if (dishWeekday == 2) {
+                dishDay.innerHTML = "Tisdagar"
+                tuesdayDiv.appendChild(dishWrapper)
+                adminDishDiv.appendChild(dishDay)
+                dishWrapper.appendChild(adminDishDiv)
+            } else if (dishWeekday == 3) {
+                dishDay.innerHTML = "Onsdagar"
+                wednesdayDiv.appendChild(dishWrapper)
+                adminDishDiv.appendChild(dishDay)
+                dishWrapper.appendChild(adminDishDiv)
+            } else if (dishWeekday == 4) {
+                dishDay.innerHTML = "Torsdagar"
+                thursdayDiv.appendChild(dishWrapper)
+                adminDishDiv.appendChild(dishDay)
+                dishWrapper.appendChild(adminDishDiv)
+            } else if (dishWeekday == 5) {
+                dishDay.innerHTML = "Fredagar"
+                fridayDiv.appendChild(dishWrapper)
+                adminDishDiv.appendChild(dishDay)
+                dishWrapper.appendChild(adminDishDiv)
+            } else if (dishWeekday == 6) {
+                dishDay.innerHTML = "Lördagar"
+                saturdayDiv.appendChild(dishWrapper)
+                adminDishDiv.appendChild(dishDay)
+                dishWrapper.appendChild(adminDishDiv)
+            } else if (dishWeekday == 0) {
+                dishDay.innerHTML = "Söndagar"
+                sundayDiv.appendChild(dishWrapper)
+                adminDishDiv.appendChild(dishDay)
+                dishWrapper.appendChild(adminDishDiv)
+            }
+
+        })
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+async function changePage(id) {
+    window.location=`./edit.html?id=${id}`
+}
---
+++ projekt-front-admin/src/edit.js
@@ -0,0 +1,94 @@
+window.addEventListener("load", async () => {
+
+    fetchOrder()
+    let sendUpdate = document.getElementById("sendUpdate")
+    sendUpdate.addEventListener("click", updateQuery)
+
+})
+
+async function fetchOrder() {
+
+    let params = new URLSearchParams(document.location.search)
+    let id = params.get("id");
+
+    try {
+        let db = await fetch(`http://127.0.0.1:3000/add/${id}`)
+        const result = await db.json()
+        console.log(result)
+
+
+        let dishnameInput = document.getElementById("dishname")
+        dishnameInput.value = result.dishname
+
+        let ingredientsInput = document.getElementById("ingredients")
+        ingredientsInput.value = result.ingredients
+
+        let allergensInput = document.getElementById("allergens")
+        allergensInput.value = result.allergens
+
+        let priceInput = document.getElementById("price")
+        priceInput.value = result.price
+
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+
+async function updateQuery(event) {
+    event.preventDefault()
+    console.log("Uppdaterar")
+    let params = new URLSearchParams(document.location.search)
+    let id = params.get("id")
+
+    const errors = []
+    errors.length = 0
+
+    const errorList = document.getElementById("errorList")
+    errorList.innerHTML = ""
+
+
+
+    let dishname = document.getElementById("dishname").value
+    let ingredients = document.getElementById("ingredients").value
+    let allergens = document.getElementById("allergens").value
+    let diet = document.getElementById("diet").value
+    let price = document.getElementById("price").value
+    let weekday = document.getElementById("weekday").value
+
+
+    let formData = {
+        dishname,
+        ingredients,
+        allergens,
+        diet,
+        price,
+        weekday
+    }
+
+    try {
+        let db = await fetch(`http://127.0.0.1:3000/add/${id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(formData)
+        });
+
+        let result = await db.json();
+
+        if (!db.ok) {
+            
+            result.errors.forEach(error => {
+                let errorLine = document.createElement("li")
+                errorLine.innerHTML = error
+                errorList.appendChild(errorLine)
+            })
+            return;
+        }
+
+        window.location = `./add.html`
+    } catch (err) {
+        console.log(err)
+    }
+}
